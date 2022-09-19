246820a07086543b111f905db8d3777c72bb9389
Putri Khairinnisa <put.nisa0103@gmail.com>
2022-09-19 16:48:44 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Putri Khairinnisa
+             Saya Tinggal di Daerah Cibarusah
+             Saya Sangat Suka Jalan-Jalan";
+    }
+}
