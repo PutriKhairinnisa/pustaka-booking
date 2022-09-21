25b87788a992fe27c64c520d0ce4dd778a9d1bc5
Putri Khairinnisa <put.nisa0103@gmail.com>
2022-09-21 19:31:13 +0700

    add view-latihan1

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>LATIHAN 1</title>
+    </head>
+    <body>
+        Hollaw bestie... Yuk kita belajar Web Programming... Sangat Menyenangkan sekali lochh !!! <br>
+        Nilai 1 = <?= $nilai1; ?>
+        Nilai 2 = <?= $nilai2; ?>
+        Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 ." + ". $nilai2 ." = ". $hasil; ?>
+    </body>
+</html>
